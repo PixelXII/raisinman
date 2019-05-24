585ee46e500000ce5965dfafa7c5ec5f796559d9
altD <35429528+PixelXII@users.noreply.github.com>
2019-05-24 08:53:45 -0700

    upload main, index and classes
    index.html, main.js, classes.js

---
+++ classes.js
@@ -0,0 +1,10 @@
+class DialogueBox {
+     constructor(content, options) {
+          this.content = content
+          this.options = options
+          this.p5Elm = rect(window.innerWidth-80, window.innerHeight-80)
+     }
+     show() {
+          
+     }
+}
---
+++ main.js
@@ -0,0 +1,10 @@
+const Gamedata = loadJSON('assets/gamedata.json')
+console.log(Gamedata)
+
+function setup() {
+     createCanvas(window.innerWidth-40, window.innerHeight-40)
+}
+
+function draw() {
+     
+}
