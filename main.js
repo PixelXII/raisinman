f3cc913f7157cde3b2204c965a26cbb7575e0a9f
altD <35429528+PixelXII@users.noreply.github.com>
2019-05-24 15:40:47 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-const Gamedata = loadJSON('assets/gamedata.json')
-console.log(Gamedata)
+const gamedatauri = 'https://cdn.jsdelivr.net/gh/PixelXII/topdownRPG@master/assets/Gamedata.json'
+var Gamedata;
+var Dbox;
+document.body.style.margin = "5px"
+
+var events = {
+  dialoguechange: new Event('dchange')
+}
+
+async function loadGameData() {
+     const response = await fetch(gamedatauri)
+     let d = await response.json().then(res => {
+          console.log("Gamedata.json loaded")
+          Gamedata = res
+     }).catch(err => {
+          console.warn("Could not load game data file")
+     })
+}
 
 function setup() {
-     createCanvas(window.innerWidth-40, window.innerHeight-40)
+     createCanvas(window.innerWidth-10, window.innerHeight-10)
+     loadGameData()
+    // Dbox = new DialogueBox('Save the princess please')
+     canvas.style.position = 'absolute'
 }
 
 function draw() {
-     
+     background(51)
 }
